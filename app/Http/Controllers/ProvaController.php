5ea144cb71b25c1e7445f8c6db4cff143255b440
Matteo Morganti <matteo99morganti@gmail.com>
2022-04-28 15:12:28 +0200

    Aggiunta vista show

--- app/Http/Controllers/ProvaController.php
+++ app/Http/Controllers/ProvaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Test;
 use Illuminate\Http\Request;
 
 class ProvaController extends Controller
@@ -43,9 +44,9 @@ class ProvaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Test $test)
     {
-        //
+        return view('prova.show', compact('test'));
     }
 
     /**
